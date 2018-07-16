f220bbef202cc9cb28596c4cff3ca59c90ea3a14
jicjjang <jicjjang12@gmail.com>
2018-07-16 23:52:48 +0900

    Initialized codes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+import './css/index.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <canvas id="signature-pad" className="signature-pad" />
+        </div>
+        <div>
+          <button id="save">Save</button>
+          <button id="clear">Clear</button>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
